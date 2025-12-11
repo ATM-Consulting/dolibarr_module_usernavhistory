7c9a32b8c216bed9f9d4097c28cddbecdbdcda13
Maxime Kohlhaas <maxime@atm-consulting.fr>
2025-12-11 16:07:50 +0100

    Fix css comment

--- css/usernavhistory.css.php
+++ css/usernavhistory.css.php
@@ -1,10 +1,10 @@
 <?php
 
-/* Copyright (C) 2025 - ATM Consulting <support@atm-consulting.fr>
+/* Copyright (C) 2025 ATM Consulting support@atm-consulting.fr
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
 /**
